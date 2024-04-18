5a44e098fd07974d36c537f899eaeef4cd813a9b
Bruno Caciano <brunocaciano@alunos.utfpr.edu.br>
2024-04-18 14:14:45 -0300

    Fixando POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "123456789", modelo: "Modelo 2", imei: "2222222", memoria: 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Fre-Fire");
